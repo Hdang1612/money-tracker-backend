1daae3290de03ff352c3a29ebd342fa8573c311b
Hai Dang <tohaidang161@gmail.com>
2024-11-26 17:40:39 +0700

    refactor: Refactor index file
    hash password and decode

--- controller/userController.js
+++ controller/userController.js
@@ -1,16 +1,22 @@
+import bcrypt from "bcrypt"
 import User from "../model//userModel.js"
 
 
 export const signUp = async (req,res) => {
     try {
-        const userData = new User(req.body)
-        const {email} =userData;
-
-        const userExist = await User.findOne({email})
+        const {email,password} =req.body
+        const userExist = await User.findOne({email}) // trả về object có email trùng hoặc null nếu ko có 
         if (userExist) {
             return res.status(400).json({message: "user already exists"})
         }
-        const saveUser = await userData.save();
+         // Hash mật khẩu trước khi lưu
+         const saltRounds = 10; 
+         const hashedPassword = await bcrypt.hash(password, saltRounds);
+         const userData = new User({
+             ...req.body, 
+             password: hashedPassword, 
+         });
+        const saveUser = await userData.save(); //lưu vào cơ sở dữ liệu
         res.status(200).json(saveUser)
     } catch (error) {
         res.status(500).json({error:"Internal Server error."})
@@ -24,8 +30,8 @@ export const logIn = async (req,res) => {
         if (!userExist) {
           return res.status(404).json({ message: "User does not exist" });
         }
-        const checkPwd=userExist.password===password
-        if (!checkPwd) {
+        const isPasswordValid = await bcrypt.compare(password, userExist.password);
+        if (!isPasswordValid) {
             return res.status(401).json({message: "Invalid password"})
         }
         return res.status(200).json({message: "Login successful",userData:userExist})
@@ -36,7 +42,7 @@ export const logIn = async (req,res) => {
     }
 }
 
-export const fetch = async (req , res ) => {
+export const fetchUsers = async (req , res ) => {
     try {
         const users = await User.find();
         if(users.length === 0) {
@@ -51,11 +57,13 @@ export const fetch = async (req , res ) => {
 export const update= async (req, res) => {
     try {
         const id = req.params.id
+        // const newPwd=req.body.password
         const userExist = await User.findOne({_id:id})
         if (!userExist) {
             return res.status(404).json({message:"User Not Found"})
         }
         const updateUser = await User.findByIdAndUpdate(id,req.body,{new:true})
+        // new:true --> hàm trả về bản ghi sau khi cập nhật 
         res.status(200).json(updateUser)
     } catch (error) {
         res.status(500).json({error:"Internal Server error."})
--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 import express from "express";
-import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import route from "./routes/userRoute.js";
 
+import connectDB from "./config/db.js";
+
 const app = express();
-
-app.use(bodyParser.json());
-dotenv.config();
+app.use(bodyParser.json()); // mọi yêu cầu http đều đi qua middleware này trước khi được route handler xử lý ,
+//nếu ko có thì req.body -- > undefined
+dotenv.config(); // trả về process.env chưa các hằng
 const PORT = process.env.PORT || 5000;
-const MONGOURL = process.env.MONGO_URL;
+connectDB();
+app.listen(PORT, () => {
+  //chạy server
+  console.log("server is running on port ", PORT);
+});
 
-mongoose
-  .connect(MONGOURL)
-  .then(() => {
-    console.log("connected successful");
-    app.listen(PORT, () => {
-      console.log("server is running on port ", PORT);
-    });
-  })
-  .catch((error) => console.log(error));
-
-  app.use("/api/user",route)
+app.use("/api/user", route);
